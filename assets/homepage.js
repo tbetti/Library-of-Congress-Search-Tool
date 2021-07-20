992920859401b14ce953b5a5c4af2322227068a7
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-07-20 11:43:53 -0400

    When search button clicked, redirect to results page. Print URL data to console

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,23 +1,18 @@
 
+var redirectUrl = "./search-page.html";
+
 $("button").click(formSubmit);
 function formSubmit(event) {
     event.preventDefault();
-
-    var search = splitString($("#search").val());
-    var format = $("#format").val();
     
+    var search = $("#search").val();
+    var format = $("#format").val();
     if (search) {
-        var url = "https://www.loc.gov/" + format + "/?q=" + search + "&fo=json";
-        console.log(url);
+        window.sessionStorage.setItem("search", search);
+        window.sessionStorage.setItem("format", format);
+
+        document.location.replace(redirectUrl);
     } else {
         alert("Please enter seach keyword(s)");
     }
 };
-
-// For multi-word searches, join each word with "-"
-// Example: New York --> New-York
-function splitString(string){
-    string = string.split(" ");
-    string = string.join("-");
-    return string;
-}
