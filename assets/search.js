adbd080f2b887cd6296d14b66e458227aaa963e5
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-07-21 10:25:04 -0400

    Created cards for first five search results and filled with data

--- assets/search.js
+++ assets/search.js
@@ -1,3 +1,4 @@
+// Take search results from home page and apply to current search
 var search = window.sessionStorage.getItem("search");
 var format = window.sessionStorage.getItem("format");
 
@@ -5,7 +6,8 @@ var results = $("#search-results");
 var searchKeyword = $("#search-keyword");
 searchKeyword.text(search);
 
-var url = "https://www.loc.gov/" + format + "/?q=" + splitString(search) + "&fo=json";
+// Use search and format results to fetch data from url
+var url = "https://www.loc.gov/" + format + "/?q=" + splitString("women's rights") + "&fo=json";
 console.log(url);
 
 fetch(url)
@@ -14,6 +16,15 @@ fetch(url)
     })
     .then(function(data){
         console.log(data);
+
+        for (var i = 0; i < 5; i++){
+            var title = data.results[i].title;
+            var date = data.results[i].date;
+            var description = data.results[i].description[0];
+            var subject = data.results[i].subject;
+             var url = data.results[i].url;
+            createCard(title, date, subject, description, url);
+        }
     })
 
 // For multi-word searches, join each word with "-"
@@ -22,4 +33,26 @@ function splitString(string){
     string = string.split(" ");
     string = string.join("-");
     return string;
+}
+
+function createCard(title, date, subject, description, url){
+    var searchCard = $("<div>").attr("class", "search-card");
+
+    var searchTitle = $("<h3>").attr("class", "search-title").text(title);
+
+    var dateHeading = $("<p>").attr("style", "font-weight: bold").text("Date: ");
+    var subjectHeading = $("<p>").attr("style", "font-weight: bold").text("Subjects: ");
+    var descriptionHeading = $("<p>").attr("style", "font-weight: bold").text("Description: ");
+
+    var dateResult = $("<span>").attr("style", "font-weight: normal").text(date);
+    var subjectResult = $("<span>").attr("style", "font-weight: normal").text(subject.join(", "));
+    var descriptionResult = $("<span>").attr("style", "font-weight: normal").text(description);
+    var seeMoreButton = $("<button>").attr("src", url).attr("class", "see-more-btn").attr("target", "_blank").text("Read More");
+    
+    dateHeading.append(dateResult);
+    subjectHeading.append(subjectResult);
+    descriptionHeading.append(descriptionResult);
+
+    results.append(searchCard);
+    searchCard.append(searchTitle).append(dateHeading).append(subjectHeading).append(descriptionHeading).append(seeMoreButton);
 }
