f7c60890524434f92dd4c4b0246fdbccfcc37d3b
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-07-22 07:58:26 -0400

    Fixed button links when users want to read more; linked search page back to homepage

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,14 +1,23 @@
-// Take search results from home page and apply to current search
-var search = window.sessionStorage.getItem("search");
-var format = window.sessionStorage.getItem("format");
-
+// Global variables
 var results = $("#search-results");
 var resultsContainer = $("#results-container");
 var searchKeyword = $("#search-keyword");
 
+// Take search results from home page and apply to current search
+var search = window.sessionStorage.getItem("search");
+var format = window.sessionStorage.getItem("format");
+
 // Set up page when redirected or refreshed
 searchKeyword.text(search);
 fetchData(format, search);
+
+//When return button clicked, go back to homepage
+$("#return").click(function(event){
+    event.preventDefault();
+
+    var redirectUrl = "index.html";
+    document.location.replace(redirectUrl);
+})
 
 // When submit button clicked again, change search and format values
 $("#submit").click(formSubmit);
@@ -100,9 +109,11 @@ function createCard(title, date, subject, description, url){
     var descriptionResult = $("<span>").attr("style", "font-weight: normal").text(description);
     descriptionHeading.append(descriptionResult);
     //see more link
-    var seeMoreButton = $("<button>").attr("src", url).attr("class", "see-more-btn").attr("target", "_blank").text("Read More");
+    var seeMoreLink = $("<a>").attr("href", url).attr("target", "_blank");
+    var seeMoreButton = $("<button>").attr("class", "see-more-btn").text("Read More");
+    seeMoreLink.append(seeMoreButton);
     
 
     resultsContainer.append(searchCard);
-    searchCard.append(searchTitle).append(dateHeading).append(subjectHeading).append(descriptionHeading).append(seeMoreButton);
+    searchCard.append(searchTitle).append(dateHeading).append(subjectHeading).append(descriptionHeading).append(seeMoreLink);
 }
